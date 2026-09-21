e1c1e1137b528d78ea3cbb50429e4fb3125013d4
RG <rampage1337pwnz@gmail.com>
2026-09-21 16:46:29 +0300

    translate-y-[2px]

--- frontend/src/pages/Admin/Users.tsx
+++ frontend/src/pages/Admin/Users.tsx
@@ -506,7 +506,7 @@ export const Users = () => {
             title="Пользователи"
           >
             <span
-              className="inline-block align-middle -translate-y-[1px]"
+              className="inline-block align-middle -translate-y-[2px]"
               aria-hidden="true"
             >
               👥
